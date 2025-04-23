3173d50e89444876b25db724cdabe59c6d6cb8c9
mndiweni04 <mndiweni04@gmail.com>
2025-04-23 19:04:05 +0200

    Added download document button
    1.Added a download document button to allow users to access my resume.
    2.Added the pdf to the public folder

--- client/src/pages/cover-page.js
+++ client/src/pages/cover-page.js
@@ -18,6 +18,17 @@ function CoverPg(){
                     My journey into the world of IT began back in my 10th grade year (2020), I have been fond of the journey and all the experiences ever since. 
                     Although I have no experience in the IT workplace I believe I can be a great addition to a team of young employees.
                 </p>
+                <button onClick={() => {
+                        const link = document.createElement('a');
+                        link.href = '/Mandla_Ndiweni.pdf';
+                        link.download = 'Mandla_Ndiweni.pdf';
+                        document.body.appendChild(link);
+                        link.click();
+                        document.body.removeChild(link);
+                    }}
+                    className="btnDownload" >
+                Download Document
+                </button>
             </div>
         </div>
     );
